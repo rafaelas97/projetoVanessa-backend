369608de906846ece3eb0b3954c4bca634cada5e
Gabriela <gabrielameloch04@gmail.com>
2025-05-15 20:46:59 -0300

    conexao front e back ok

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Permite JSON no corpo das requisições
+app.use(express.static(path.join(__dirname, "../frontend/src")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/src/index.html"));
+});
+
+app.get("/parques", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/src/parques.html"));
+});
+
+app.get("/reserva", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/src/reserva.html"));
+});
+
+app.get("/feedback", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/src/feedback.html"));
+});
+
+// ROTA POST para receber reservas
+app.post("/reserva", (req, res) => {
+  const { nome, parque, data } = req.body;
+  console.log("Reserva recebida:", { nome, parque, data });
+
+  res.status(200).json({ mensagem: "Reserva recebida com sucesso!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+app.post("/feedback", (req, res) => {
+  const { nome, email, mensagem } = req.body;
+  console.log("Feedback recebido:", { nome, email, mensagem });
+
+  res.status(200).json({ mensagem: "Feedback recebido com sucesso!" });
+});
