cbfd591a8169add2946d44a54ffd805e9be7fff5
rafaelas97 <rafaelasleonidas@gmail.com>
2025-05-19 16:43:58 -0300

    feedback e reserva OK no terminal

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ require('./src/database/connection');
 app.use(cors());
 app.use(express.json()); // Permite JSON no corpo das requisições
 app.use(express.static(path.join(__dirname, "../frontend/src")));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/src/index.html"));
@@ -27,8 +29,12 @@ app.get("/feedback", (req, res) => {
 
 // ROTA POST para receber reservas
 app.post("/reserva", (req, res) => {
-  const { nome, parque, data } = req.body;
-  console.log("Reserva recebida:", { nome, parque, data });
+  const { nome, parque, data, horario } = req.body;
+  console.log("Reserva recebida:", req.body);
+  console.log("Nome:", nome);
+  console.log("Parque:", parque);
+  console.log("Data:", data);
+  console.log("Horário:", horario);
 
   res.status(200).json({ mensagem: "Reserva recebida com sucesso!" });
 });
@@ -37,9 +43,10 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 app.post("/feedback", (req, res) => {
-  const { nome, email, mensagem } = req.body;
-  console.log("Feedback recebido:", { nome, email, mensagem });
+  const { nome, mensagem } = req.body;
+  console.log("Feedback recebido:", { nome, mensagem });
 
   res.status(200).json({ mensagem: "Feedback recebido com sucesso!" });
 });
